726808779d292fe9d99e8f2d88c155022f94472c
H4RDCORE <kan_off@mail.ru>
2020-02-21 01:03:46 +0300

    last add -ggs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [ 
+        {
+          name: 'Vazovskyi',
+          id: '1',
+          email: 'alias@mac.com'
+        },
+        {
+          name: 'Drakula',
+          id: '2',
+          email: 'cyrus@gmail.com'
+        }, 
+        {
+          name: 'Zombie',
+          id: '3',
+          email: 'tbusch@aol.com'
+        },
+        {
+          name: 'Beatrix',
+          id: '4',
+          email: 'openldap@aol.com'
+        },
+        {
+          name: 'Batman',
+          id: '5',
+          email: 'janneh@msn.com'
+        }, 
+        {
+          name: 'Skywalker',
+          id: '6',
+          email: 'mrdvt@msn.com'
+        },
+        {
+          name: 'Deadpool',
+          id: '7',
+          email: 'goresky@outlook.com'
+        }, 
+        {
+          name: 'Spiderman',
+          id: '8',
+          email: 'sbmrjbr@mac.com'
+        },{
+          name: 'Darthwayder',
+          id: '9',
+          email: 'fudrucker@mac.com'
+        }, 
+        {
+          name: 'Chubaka',
+          id: '10',
+          email: 'tskirvin@icloud.com'
+        }
+      ],
+      searchFilter: ''
+    };
+  } 
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => {
+    let x = response.json();
+    console.log(x);
+  }
+  ); 
+}
+  handleChange = (e) => { 
+    this.setState({ searchFilter: e.target.value })
+  };
+  render() {
+    const {monsters, searchFilter} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchFilter.toLowerCase()))
+    return (
+      <div className="App">
+        <h1>The Monsters</h1>
+        <SearchBox placeholder='search monsters' handleChange={ this.handleChange } />
+        <CardList monsters={filteredMonsters} />
+      </div> 
+    )
+  }
 }
 
 export default App;
